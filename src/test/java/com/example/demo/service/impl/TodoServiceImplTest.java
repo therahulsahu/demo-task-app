a19574eb706474072d7ab7ef1a94d6a50389ae7b
rahul sahu <therahulsahu7@gmail.com>
2023-12-14 01:19:32 +0530

    reading test properties from a test profile

--- src/test/java/com/example/demo/service/impl/TodoServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/TodoServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -12,17 +13,18 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 3030)
+@ActiveProfiles("test")
 class TodoServiceImplTest {
 
     @Autowired
     private TodoServiceImpl todoService;
 
     // changing application properties to change the base url of our client
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("app.todoUrl", () -> "http://localhost:3030");
-        registry.add("spring.cloud.openfeign.client.config.default.loggerLevel", () -> "full");
-    }
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("app.todoUrl", () -> "http://localhost:3030");
+//        registry.add("spring.cloud.openfeign.client.config.default.loggerLevel", () -> "full");
+//    }
 
     @BeforeEach
     void setUp() {
